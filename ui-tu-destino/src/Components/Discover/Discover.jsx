eefddcf233a68223a564185476495633a037969a
Kristiamnx <rivaskristopher24@gmail.com>
2024-10-07 15:22:38 -0500

    structuring of discover section

--- ui-tu-destino/src/Components/Discover/Discover.jsx
+++ ui-tu-destino/src/Components/Discover/Discover.jsx
@@ -1,11 +1,82 @@
 import Gallery from './Gallery'
 import ButtonPanel from './ButtonPanel'
+const obj = [
+  {
+    name: "Algun parque de Medellin",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/naturaleza/Botanico/jjjrls8pkaskjz4stuvd.jpg",
+  },
+  {
+    name: "Nose como se llama este...",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/cultura/Palacio%20Rafael%20Uribe%20Cultura/hpi6dcccsvmnbpp2pzce.jpg",
+  },
+  {
+    name: "Museo el castillo",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/historia/MuseoCastillo/op4ahaps1idu5uvptbwg.jpg",
+  },
+  {
+    name: "Logo Perron de Tu Destino",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/den1krumk48nfabnwiir.jpg",
+  },
+
+  {
+    name: "Logo Perron de Tu Destino",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/den1krumk48nfabnwiir.jpg",
+  },
+
+  {
+    name: "Logo Perron de Tu Destino",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/den1krumk48nfabnwiir.jpg",
+  },
+
+  {
+    name: "Logo Perron de Tu Destino",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/den1krumk48nfabnwiir.jpg",
+  },
+  {
+    name: "Algun parque de Medellin",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/naturaleza/Botanico/jjjrls8pkaskjz4stuvd.jpg",
+  },
+  {
+    name: "Nose como se llama este...",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/cultura/Palacio%20Rafael%20Uribe%20Cultura/hpi6dcccsvmnbpp2pzce.jpg",
+  },
+  {
+    name: "Museo el castillo",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/historia/MuseoCastillo/op4ahaps1idu5uvptbwg.jpg",
+  },
+  {
+    name: "Algun parque de Medellin",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/naturaleza/Botanico/jjjrls8pkaskjz4stuvd.jpg",
+  },
+  {
+    name: "Nose como se llama este...",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/cultura/Palacio%20Rafael%20Uribe%20Cultura/hpi6dcccsvmnbpp2pzce.jpg",
+  },
+  {
+    name: "Museo el castillo",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/historia/MuseoCastillo/op4ahaps1idu5uvptbwg.jpg",
+  },
+  {
+    name: "Algun parque de Medellin",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/naturaleza/Botanico/jjjrls8pkaskjz4stuvd.jpg",
+  },
+  {
+    name: "Nose como se llama este...",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/cultura/Palacio%20Rafael%20Uribe%20Cultura/hpi6dcccsvmnbpp2pzce.jpg",
+  },
+  {
+    name: "Museo el castillo",
+    img: "https://res.cloudinary.com/dhtmy6izv/image/upload/f_png/Multimedia/Lugares/historia/MuseoCastillo/op4ahaps1idu5uvptbwg.jpg",
+  },
+];
+
+
 function Discover() {
   
   return(
     <section className='w-full h-full bg-slate-950 flex flex-row'>
     <ButtonPanel/>
-    <Gallery/>
+    <Gallery places={obj}/>
     
     </section>
   )
