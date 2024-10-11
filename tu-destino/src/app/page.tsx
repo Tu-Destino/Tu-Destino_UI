d13cff938d77d08ef548b144a36533ddc9ef1435
Juanzch <juanmacardenas.co@gmail.com>
2024-10-11 01:55:38 -0500

    Responsive nav completion, and full implementation of the hamburger menu

--- tu-destino/src/app/page.tsx
+++ tu-destino/src/app/page.tsx
@@ -7,8 +7,7 @@ import SearchBar from "../components/main/SearchBar";
 export default function Home() {
   return (<>
       <NavigationBar/>
-      <SearchBar/>
-      <Footer/>
+
    </>
   );
 }
--- tu-destino/src/components/main/nav/NavigationBar.tsx
+++ tu-destino/src/components/main/nav/NavigationBar.tsx
@@ -1,36 +1,81 @@
+"use client";
+import {
+  Modal,
+  ModalContent,
+  ModalBody,
+  useDisclosure,
+} from "@nextui-org/react";
+import React from "react";
 import Languages from "./Languages";
 import Login from "./Login";
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
+import { poppins } from "@/styles/fonts";
 
 function NavigationBar() {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <nav className="bg-white w-full h-[63px] flex flex-row border-b border-[#e0e0e0] fixed z-50">
-      <div className="md:hidden">
-        <button>
-          <MenuIcon/>
+      <div className="md:hidden w-12 flex justify-center items-center">
+        <button onClick={onOpen}>
+          <MenuIcon />
         </button>
+        <Modal
+          isOpen={isOpen}
+          placement="top-center"
+          onOpenChange={onOpenChange}
+        >
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalBody className="mt-8 mb-4 flex justify-center items-center">
+                  <Link className={`hover:bg-slate-100 w-full text-center p-3 rounded-xl ${poppins.className} font-normal`} href="/places">
+                    Lugares
+                  </Link>
+                  <Link className={`hover:bg-slate-100 w-full text-center p-3 rounded-xl  ${poppins.className} font-normal`} href="/discover">
+                    Descubrir
+                  </Link>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
-      <div className=" w-[6rem] md:w-[22%] lg:w-[35%]  h-full flex items-center ">
-        <a href="#">
+      <div className=" w-[3rem] md:w-[22%] lg:w-[35%]  h-full flex items-center ">
+        <Link href="#">
           <img
-            src="src/assets/TuDestinoLogo.png"
+            src="/logoYourDestiny.png"
             alt="Logo Tu Destino"
-            className="ml-12  w-28 "
+            className="hidden md:flex ml-12  w-28 "
           />
-        </a>
+          <img
+            src="/logoSmall.png"
+            alt="Logo Tu Destino"
+            className="md:hidden  w-12 "
+          />
+        </Link>
       </div>
-      <div className="bg-slate-300 w-[30%] h-full"></div>
-      <div className="flex md:w-[48%] lg:w-[35%] h-full  items-center justify-around ">
+      <div className="bg-slate-300 flex-grow md:w-[30%] h-full"></div>
+      <div className="flex w-[9.5rem] md:w-[48%] lg:w-[35%] h-full  items-center justify-around ">
         <div className="hidden md:flex flex-1 justify-around">
-          <a className="w-28 h-10 flex items-center justify-center rounded-3xl hover:bg-slate-100" href="">Descubrir</a>
-          <a className="w-24 h-10 flex items-center justify-center rounded-3xl hover:bg-slate-100" href="">Lugares</a>
+          <Link
+            className="w-28 h-10 flex items-center justify-center rounded-3xl hover:bg-slate-100"
+            href="/discover"
+          >
+            Descubrir
+          </Link>
+          <Link
+            className="w-24 h-10 flex items-center justify-center rounded-3xl hover:bg-slate-100"
+            href="/places"
+          >
+            Lugares
+          </Link>
         </div>
         <div className="flex">
-          <Languages/>
-          <Login/>
+          <Languages />
+          <Login />
         </div>
-        
-
       </div>
     </nav>
   );
