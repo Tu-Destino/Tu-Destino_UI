54c0e79c2e5bd14e2e7fc7e8f8e04013aa95a5bb
Juanzch <juanmacardenas.co@gmail.com>
2024-12-17 14:24:38 -0500

    Implementation of all

--- tu-destino/src/redux/listTitles/thunks.ts
+++ tu-destino/src/redux/listTitles/thunks.ts
@@ -1,8 +1,7 @@
 import { AppDispatch } from "../store";
 import { setListTitle, startLoadingList } from "./listTitlesSlice";
 
-// const urlBase = "https://tudestinoresourse.coalmd.com/TD/api/v1"
-const urlBase = 'https://bl-monolith-td.onrender.com/TD/api/v1'
+ const urlBase = "https://tudestinoresourse.coalmd.com/TD/api/v1"
 
 export const getListTitle = () => {
   return async (dispatch: AppDispatch) => {
